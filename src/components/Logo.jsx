11826cc115ee7ef10fef7e929bcaeabb1cca30f8
Gourav Malviya <gouravmalviya445@gmail.com>
2024-03-26 19:46:09 +0530

    updating files

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Logo = ({ className = '' }) => {
     return (
         <div className='flex flex-wrap items-center'>
-            <img className={`rounded-full w-12 ${className}`} src="/images/logo.png" alt="blog" />
+            <img className={`rounded-full w-12 ${className}`} src="https://i.postimg.cc/rFbmmK5n/logo.png" alt="blog" />
         </div>
     )
 }
